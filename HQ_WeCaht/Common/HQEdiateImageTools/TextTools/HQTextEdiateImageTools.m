61f1cd0f40da0770bb655412c471902fda1ed834
huangqizhan <757618403@qq.com>
2017-08-17 17:36:17 +0800

    1

--- HQ_WeCaht/Common/HQEdiateImageTools/TextTools/HQTextEdiateImageTools.m
+++ HQ_WeCaht/Common/HQEdiateImageTools/TextTools/HQTextEdiateImageTools.m
@@ -9,19 +9,138 @@
 #import "HQTextEdiateImageTools.h"
 #import "HQEdiateImageController.h"
 
+@interface HQTextEdiateImageTools ()
+
+
+@property (nonatomic) UIView *drawMenuView;
+@property (nonatomic) UIImageView *drawImageView;
+@property (nonatomic) UISlider *colorSlider;
+
+
+@end
+
+
+
 @implementation HQTextEdiateImageTools
 
 - (instancetype)initWithEdiateController:(HQEdiateImageController *)ediateController andEdiateToolInfo:(HQEdiateToolInfo *)toolInfo{
     return [super initWithEdiateController:ediateController  andEdiateToolInfo:toolInfo];
 }
-
 - (void)setUpCurrentEdiateStatus{
     [super setUpCurrentEdiateStatus];
+    _drawMenuView =  [[UIView alloc] initWithFrame:CGRectMake(0, APP_Frame_Height, App_Frame_Width, 80)];
+    _drawMenuView.backgroundColor = [UIColor clearColor];//BOTTOMBARCOLOR
+    UIButton *cancelBut = [[UIButton alloc] initWithFrame:CGRectMake(10, 5, 40, 40)];
+    [cancelBut setImage:[UIImage imageNamed:@"EdiateImageDismissBut"] forState:UIControlStateNormal];
+    [cancelBut addTarget:self action:@selector(clearDrawViewButtonAction:) forControlEvents:UIControlEventTouchUpInside];
+    [_drawMenuView addSubview:cancelBut];
+    
+    UIButton *addButton  = [[UIButton alloc] initWithFrame:CGRectMake(App_Frame_Width-50, 5, 40, 40)];
+    [addButton setImage:[UIImage imageNamed:@"addActionIcon"] forState:UIControlStateNormal];
+    [addButton addTarget:self action:@selector(addButtonAction:) forControlEvents:UIControlEventTouchUpInside];
+    [_drawMenuView addSubview:addButton];
+    
+    [self.imageEdiateController.view addSubview:_drawMenuView];
+    
+    [self setMenuView];
+
+    [UIView animateWithDuration:0.15 delay:0 usingSpringWithDamping:0.8 initialSpringVelocity:0 options:UIViewAnimationOptionTransitionCurlDown animations:^{
+        _drawMenuView.top = APP_Frame_Height- 80;
+    } completion:nil];
+    
+}
+- (void)setMenuView{
+    _colorSlider = [self defaultSliderWithWidth:_drawMenuView.width - 40];
+    _colorSlider.left = 20;
+    _colorSlider.top  = 40;
+    [_colorSlider addTarget:self action:@selector(colorSliderDidChange:) forControlEvents:UIControlEventValueChanged];
+    _colorSlider.backgroundColor = [UIColor colorWithPatternImage:[self colorSliderBackground]];
+    _colorSlider.value = 0;
+    [_drawMenuView addSubview:_colorSlider];
+    
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake((App_Frame_Width-100)/2.0, 5, 100, 20)];
+    title.text = @"滑动调整色值";
+    title.textAlignment = NSTextAlignmentCenter;
+    title.font = [UIFont systemFontOfSize:14];
+    title.textColor = CANCELBUTTONCOLOR;
+    [_drawMenuView addSubview:title];
+    
+    
 }
 
+- (UISlider*)defaultSliderWithWidth:(CGFloat)width{
+    UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake(0, 0, width, 30)];
+    
+    [slider setMaximumTrackImage:[UIImage new] forState:UIControlStateNormal];
+    [slider setMinimumTrackImage:[UIImage new] forState:UIControlStateNormal];
+    [slider setThumbImage:[UIImage new] forState:UIControlStateNormal];
+    slider.thumbTintColor = [UIColor whiteColor];
+    
+    return slider;
+}
+- (void)clearDrawViewButtonAction:(UIButton *)sender{
+    [self clearCurrentEdiateStatus];
+    self.imageEdiateController.scrollView.panGestureRecognizer.minimumNumberOfTouches = 1;
+    [UIView animateWithDuration:0.15 delay:0 usingSpringWithDamping:0.8 initialSpringVelocity:0 options:UIViewAnimationOptionTransitionCurlDown animations:^{
+        _drawMenuView.top = APP_Frame_Height;
+    } completion:^(BOOL finished){
+        [_drawMenuView removeFromSuperview];
+        [_drawImageView removeFromSuperview];
+        [self.imageEdiateController resetBottomViewEdiateStatus];
+    }];
+}
+- (void)addButtonAction:(UIButton *)sender{
+    
+}
+- (void)colorSliderDidChange:(UISlider *)slider{
+    
+}
 - (void)clearCurrentEdiateStatus{
     [super clearCurrentEdiateStatus];
 }
+- (UIImage*)colorSliderBackground{
+    CGSize size = _colorSlider.frame.size;
+    
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0.0);
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGRect frame = CGRectMake(5, (size.height-10)/2, size.width-10, 5);
+    CGPathRef path = [UIBezierPath bezierPathWithRoundedRect:frame cornerRadius:5].CGPath;
+    CGContextAddPath(context, path);
+    CGContextClip(context);
+    
+    CGColorSpaceRef colorSpaceRef = CGColorSpaceCreateDeviceRGB();
+    CGFloat components[] = {
+        0.0f, 0.0f, 0.0f, 1.0f,
+        1.0f, 1.0f, 1.0f, 1.0f,
+        1.0f, 0.0f, 0.0f, 1.0f,
+        1.0f, 1.0f, 0.0f, 1.0f,
+        0.0f, 1.0f, 0.0f, 1.0f,
+        0.0f, 1.0f, 1.0f, 1.0f,
+        0.0f, 0.0f, 1.0f, 1.0f
+    };
+    
+    size_t count = sizeof(components)/ (sizeof(CGFloat)* 4);
+    CGFloat locations[] = {0.0f, 0.9/3.0, 1/3.0, 1.5/3.0, 2/3.0, 2.5/3.0, 1.0};
+    
+    CGPoint startPoint = CGPointMake(5, 0);
+    CGPoint endPoint = CGPointMake(size.width-5, 0);
+    
+    CGGradientRef gradientRef = CGGradientCreateWithColorComponents(colorSpaceRef, components, locations, count);
+    
+    CGContextDrawLinearGradient(context, gradientRef, startPoint, endPoint, kCGGradientDrawsAfterEndLocation);
+    
+    UIImage *tmp = UIGraphicsGetImageFromCurrentImageContext();
+    
+    CGGradientRelease(gradientRef);
+    CGColorSpaceRelease(colorSpaceRef);
+    
+    UIGraphicsEndImageContext();
+    
+    return tmp;
+}
+
 - (void)executeWithCompletionBlock:(void (^)(UIImage *, NSError *, NSDictionary *))completionBlock{
     
 }
